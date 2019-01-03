8a14f2537c2d20d099f99a9cbc1bb4b4b7ee87e6
chase <chase@mailbox.org>
2019-01-03 11:00:30 +0100

    Delombofy Unit class

--- src/main/java/org/chase/juco/unit/Unit.java
+++ src/main/java/org/chase/juco/unit/Unit.java
@@ -1,21 +1,20 @@
 package org.chase.juco.unit;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.chase.juco.amount.Amount;
 import org.chase.juco.amount.Prefix;
+
+import java.util.Objects;
+import java.util.StringJoiner;
 
 /**
  * Represents a Unit of Measurement, consisting of a {@link Unit#name} and a {@link Unit#conversionRate}
  */
 @SuppressWarnings({"WeakerAccess", "unused"})
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
 public class Unit {
+
+    /**
+     * The {@link UnitGroup} this Unit belongs to
+     */
     private UnitGroup unitgroup;
 
     /**
@@ -56,6 +55,20 @@ public class Unit {
      */
     private double conversionRate;
 
+    @java.beans.ConstructorProperties({"unitgroup", "prefix", "baseUnit", "name", "shorthand", "conversionRate"})
+    public Unit(final UnitGroup unitgroup, final Prefix prefix, final boolean baseUnit, final String name, final String shorthand, final double conversionRate) {
+        this.unitgroup = unitgroup;
+        this.prefix = prefix;
+        this.baseUnit = baseUnit;
+        this.name = name;
+        this.shorthand = shorthand;
+        this.conversionRate = conversionRate;
+    }
+
+    public static UnitBuilder builder() {
+        return new UnitBuilder();
+    }
+
     /**
      * Returns a new Unit instance with the given Prefix. Can be used in {@link Amount#convertTo(Unit)}
      * @param prefix The {@link Prefix} to be applied
@@ -78,5 +91,150 @@ public class Unit {
      */
     public String getDisplayShort() {
         return shorthand != null && !shorthand.isEmpty() ? shorthand : name;
+    }
+
+    public UnitGroup getUnitgroup() {
+        return this.unitgroup;
+    }
+
+    public Prefix getPrefix() {
+        return this.prefix;
+    }
+
+    public boolean isBaseUnit() {
+        return this.baseUnit;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getShorthand() {
+        return this.shorthand;
+    }
+
+    public double getConversionRate() {
+        return this.conversionRate;
+    }
+
+    public void setUnitgroup(final UnitGroup unitgroup) {
+        this.unitgroup = unitgroup;
+    }
+
+    public void setPrefix(final Prefix prefix) {
+        this.prefix = prefix;
+    }
+
+    public void setBaseUnit(final boolean baseUnit) {
+        this.baseUnit = baseUnit;
+    }
+
+    public void setName(final String name) {
+        this.name = name;
+    }
+
+    public void setShorthand(final String shorthand) {
+        this.shorthand = shorthand;
+    }
+
+    public void setConversionRate(final double conversionRate) {
+        this.conversionRate = conversionRate;
+    }
+
+    @Override
+    public String toString() {
+        return new StringJoiner(", ", Unit.class.getSimpleName() + "[", "]")
+                .add("unitgroup=" + unitgroup.getName())
+                .add("prefix=" + prefix)
+                .add("baseUnit=" + baseUnit)
+                .add("name='" + name + "'")
+                .add("shorthand='" + shorthand + "'")
+                .add("conversionRate=" + conversionRate)
+                .toString();
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final Unit unit = (Unit) o;
+        return baseUnit == unit.baseUnit &&
+                Double.compare(unit.conversionRate, conversionRate) == 0 &&
+                Objects.equals(unitgroup, unit.unitgroup) &&
+                prefix == unit.prefix &&
+                Objects.equals(name, unit.name) &&
+                Objects.equals(shorthand, unit.shorthand);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(unitgroup, prefix, baseUnit, name, shorthand, conversionRate);
+    }
+
+    public static class UnitBuilder {
+        private UnitGroup unitgroup;
+        private Prefix prefix;
+        private boolean baseUnit;
+        private String name;
+        private String shorthand;
+        private double conversionRate;
+
+        UnitBuilder() {
+        }
+
+        /**
+         * @see Unit#unitgroup
+         */
+        public Unit.UnitBuilder unitgroup(final UnitGroup unitgroup) {
+            this.unitgroup = unitgroup;
+            return this;
+        }
+
+        /**
+         * @see Unit#prefix
+         */
+        public Unit.UnitBuilder prefix(final Prefix prefix) {
+            this.prefix = prefix;
+            return this;
+        }
+
+        /**
+         * @see Unit#baseUnit
+         */
+        public Unit.UnitBuilder baseUnit(final boolean baseUnit) {
+            this.baseUnit = baseUnit;
+            return this;
+        }
+
+        public Unit.UnitBuilder name(final String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Unit.UnitBuilder shorthand(final String shorthand) {
+            this.shorthand = shorthand;
+            return this;
+        }
+
+        public Unit.UnitBuilder conversionRate(final double conversionRate) {
+            this.conversionRate = conversionRate;
+            return this;
+        }
+
+        public Unit build() {
+            return new Unit(unitgroup, prefix, baseUnit, name, shorthand, conversionRate);
+        }
+
+        @Override
+        public String toString() {
+            return new StringJoiner(", ", UnitBuilder.class.getSimpleName() + "[", "]")
+                    .add("unitgroup=" + unitgroup.getName())
+                    .add("prefix=" + prefix)
+                    .add("baseUnit=" + baseUnit)
+                    .add("name='" + name + "'")
+                    .add("shorthand='" + shorthand + "'")
+                    .add("conversionRate=" + conversionRate)
+                    .toString();
+        }
     }
 }
